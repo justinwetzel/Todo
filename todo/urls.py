4eaa0e3a5ae75e859856aba8dd5124c4eeb2e2c4
vagrant <vagrant@precise32.(none)>
2015-01-03 20:02:06 +0000

    Made first HttpRespone

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'todo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+     url(r'^$', 'todo.views.hello'),
 
-    url(r'^admin/', include(admin.site.urls)),
 )
